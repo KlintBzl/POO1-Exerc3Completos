a7b0a8c88d54642fe79ad1d230912ababb42a6b3
KlintBzl <klintburzlaff@gmail.com>
2025-03-13 17:44:04 -0300

    Atividades realizadas

---
+++ Lista3Atvd4/src/lista3atvd4/Projeto.java
@@ -0,0 +1,28 @@
+
+package lista3atvd4;
+
+public abstract class Projeto {
+    private String titulo, dataInicio;
+    private double orcamento;
+    
+    public Projeto(String titulo, String dataInicio, double orcamento){
+        this.dataInicio = dataInicio;
+        this.orcamento = orcamento;
+        this.titulo = titulo;
+    }
+
+    public void setOrcamento(double orcamento) {
+        this.orcamento = orcamento;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public String getDataInicio() {
+        return dataInicio;
+    }
+    public double getOrcamento() {
+        return orcamento;
+    }
+    
+    public abstract int calcularPrazo();
+}
